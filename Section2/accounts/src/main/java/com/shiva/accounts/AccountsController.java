f10b1053d1f6916571d5fa70174ec7db9139cd13
shivam-522 <shivamsamadhiya914@gmail.com>
2025-12-12 22:44:36 +0530

    Initial commit

---
+++ Section2/accounts/src/main/java/com/shiva/accounts/AccountsController.java
@@ -0,0 +1,14 @@
+package com.shiva.accounts;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountsController {
+
+    @GetMapping("sayHello")
+    public String sayHello()
+    {
+        return "Hi World";
+    }
+}
